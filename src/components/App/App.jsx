23522d02586fbf4091d1e5a3c0d38920a857318a
Serhii Vasiliev <sepovi4@gmail.com>
2024-06-10 14:52:32 +0200

    update1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,24 @@
-export default function App() {}
+// import TaskForm from "first";
+// import Error from "first";
+// import Loader from "first";
+// import TaskList from "first";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import fetchTasks from "../../redux/tasksOps";
+
+export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+  return (
+    <main>
+      <h1>HTTP request with Redux</h1>
+      {/* <TaskForm /> */}
+      {/* <Loader>Loading messages</Loader> */}
+      {/* <Error>Error message</Error> */}
+      {/* <TaskList /> */}
+    </main>
+  );
+}
